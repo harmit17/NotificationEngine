2adc05170ebcbc87f4731fdb06637f8d713bbeb8
Ronak Chaudhari <ronak.chaudhari@peaas.co>
2020-02-27 18:11:25 +0530

    changes are done..git add .

--- src/main/java/com/finablr/platform/notification/domain/NotificationRequest.java
+++ src/main/java/com/finablr/platform/notification/domain/NotificationRequest.java
@@ -1,39 +1,47 @@
 package com.finablr.platform.notification.domain;
 
-import java.sql.Date;
+import java.time.Instant;
+import java.util.Map;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import org.springframework.lang.NonNull;
 
 @Entity
 @Table(name = "notification_request")
 public class NotificationRequest {
-	@NonNull
+	
 	@Id
+	@Column(nullable = false)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@NonNull
-	private String notificationData;
-	@NonNull
-	private String receipientDetails;
-	@NonNull
+	
+	@Column(nullable = false)
+	private Map<Integer, String> notificationData;
+	
+	@Column(nullable = false)
+	private Map<Integer, String> receipientDetails;
+	
+	@Column(nullable = false)
 	private Integer retryCount;
-	@NonNull
+	
 	private String notificationSubject;
-	@NonNull
+	
 	private String notificationBody;
-	@NonNull
+	
+	@Column(nullable = false)
 	private String status;
-	@NonNull
-	private Date lastDeliveryAttempt;
-	@NonNull
-	private Date requestTime;
-	@NonNull
+	
+	private Instant lastDeliveryAttempt;
+	
+	private Instant requestTime;
+	
+	@Column(nullable = false)
 	private Long notificationTemplateId;
+	
 	
 	/**
 	 * @return the id
@@ -41,138 +49,121 @@ public class NotificationRequest {
 	public Long getId() {
 		return id;
 	}
-	
 	/**
 	 * @param id the id to set
 	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
 	/**
 	 * @return the notificationData
 	 */
 	public String getNotificationData() {
 		return notificationData;
 	}
-	
 	/**
 	 * @param notificationData the notificationData to set
 	 */
 	public void setNotificationData(String notificationData) {
 		this.notificationData = notificationData;
 	}
-	
 	/**
 	 * @return the receipientDetails
 	 */
 	public String getReceipientDetails() {
 		return receipientDetails;
 	}
-	
 	/**
 	 * @param receipientDetails the receipientDetails to set
 	 */
 	public void setReceipientDetails(String receipientDetails) {
 		this.receipientDetails = receipientDetails;
 	}
-	
 	/**
 	 * @return the retryCount
 	 */
 	public Integer getRetryCount() {
 		return retryCount;
 	}
-	
 	/**
 	 * @param retryCount the retryCount to set
 	 */
 	public void setRetryCount(Integer retryCount) {
 		this.retryCount = retryCount;
 	}
-	
 	/**
 	 * @return the notificationSubject
 	 */
 	public String getNotificationSubject() {
 		return notificationSubject;
 	}
-	
 	/**
 	 * @param notificationSubject the notificationSubject to set
 	 */
 	public void setNotificationSubject(String notificationSubject) {
 		this.notificationSubject = notificationSubject;
 	}
-	
 	/**
 	 * @return the notificationBody
 	 */
 	public String getNotificationBody() {
 		return notificationBody;
 	}
-	
 	/**
 	 * @param notificationBody the notificationBody to set
 	 */
 	public void setNotificationBody(String notificationBody) {
 		this.notificationBody = notificationBody;
 	}
-	
 	/**
 	 * @return the status
 	 */
 	public String getStatus() {
 		return status;
 	}
-	
 	/**
 	 * @param status the status to set
 	 */
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	
 	/**
 	 * @return the lastDeliveryAttempt
 	 */
-	public Date getLastDeliveryAttempt() {
+	public Instant getLastDeliveryAttempt() {
 		return lastDeliveryAttempt;
 	}
-	
 	/**
 	 * @param lastDeliveryAttempt the lastDeliveryAttempt to set
 	 */
-	public void setLastDeliveryAttempt(Date lastDeliveryAttempt) {
+	public void setLastDeliveryAttempt(Instant lastDeliveryAttempt) {
 		this.lastDeliveryAttempt = lastDeliveryAttempt;
 	}
-	
 	/**
 	 * @return the requestTime
 	 */
-	public Date getRequestTime() {
+	public Instant getRequestTime() {
 		return requestTime;
 	}
-	
 	/**
 	 * @param requestTime the requestTime to set
 	 */
-	public void setRequestTime(Date requestTime) {
+	public void setRequestTime(Instant requestTime) {
 		this.requestTime = requestTime;
 	}
-	
 	/**
 	 * @return the notificationTemplateId
 	 */
 	public Long getNotificationTemplateId() {
 		return notificationTemplateId;
 	}
-	
 	/**
 	 * @param notificationTemplateId the notificationTemplateId to set
 	 */
 	public void setNotificationTemplateId(Long notificationTemplateId) {
 		this.notificationTemplateId = notificationTemplateId;
 	}
+	
+	
 	
 }
