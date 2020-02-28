e1cd275dee917a2fec2d77e33f8fbf96e2371d1a
Dhaval Vaitha <dhaval.vaitha@peaas.co>
2020-02-28 18:04:25 +0530

    changes of getter and setter in NotificationcontentType and ddl-auto to none in app.yml file

--- src/main/java/com/finablr/platform/notification/domain/NotificationContentType.java
+++ src/main/java/com/finablr/platform/notification/domain/NotificationContentType.java
@@ -13,8 +13,33 @@ public class NotificationContentType {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
-	
+
 	private boolean isDisable;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isDisable() {
+		return isDisable;
+	}
+
+	public void setDisable(boolean isDisable) {
+		this.isDisable = isDisable;
+	}
+
 }
