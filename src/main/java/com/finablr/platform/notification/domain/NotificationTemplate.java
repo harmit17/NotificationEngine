5df61299cfa08ab5fb52dc7fef6a468c8e7761c9
Dhaval Vaitha <dhaval.vaitha@peaas.co>
2020-03-02 14:49:03 +0530

    added getter setter in notificationtemplate.java file

--- src/main/java/com/finablr/platform/notification/domain/NotificationTemplate.java
+++ src/main/java/com/finablr/platform/notification/domain/NotificationTemplate.java
@@ -42,4 +42,94 @@ public class NotificationTemplate {
 	
 	@Column(nullable = false)
 	private Long notificationContentTypeId;
+
+	public Long getTemplateId() {
+		return templateId;
+	}
+
+	public void setTemplateId(Long templateId) {
+		this.templateId = templateId;
+	}
+
+	public String getTemplateCode() {
+		return templateCode;
+	}
+
+	public void setTemplateCode(String templateCode) {
+		this.templateCode = templateCode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getTemplateSubject() {
+		return templateSubject;
+	}
+
+	public void setTemplateSubject(String templateSubject) {
+		this.templateSubject = templateSubject;
+	}
+
+	public String getTemplateBody() {
+		return templateBody;
+	}
+
+	public void setTemplateBody(String templateBody) {
+		this.templateBody = templateBody;
+	}
+
+	public Integer getMaxRetry() {
+		return maxRetry;
+	}
+
+	public void setMaxRetry(Integer maxRetry) {
+		this.maxRetry = maxRetry;
+	}
+
+	public Instant getEffectiveForm() {
+		return effectiveForm;
+	}
+
+	public void setEffectiveForm(Instant effectiveForm) {
+		this.effectiveForm = effectiveForm;
+	}
+
+	public Instant getEffectiveTo() {
+		return effectiveTo;
+	}
+
+	public void setEffectiveTo(Instant effectiveTo) {
+		this.effectiveTo = effectiveTo;
+	}
+
+	public Long getNotificationChannelId() {
+		return notificationChannelId;
+	}
+
+	public void setNotificationChannelId(Long notificationChannelId) {
+		this.notificationChannelId = notificationChannelId;
+	}
+
+	public Long getNotificationContentTypeId() {
+		return notificationContentTypeId;
+	}
+
+	public void setNotificationContentTypeId(Long notificationContentTypeId) {
+		this.notificationContentTypeId = notificationContentTypeId;
+	}
+	
+	
 }
--- src/main/java/com/finablr/platform/notification/service/NotificationTemplateService.java
+++ src/main/java/com/finablr/platform/notification/service/NotificationTemplateService.java
@@ -1,8 +1,9 @@
 package com.finablr.platform.notification.service;
 
 public interface NotificationTemplateService {
-	
+
 	void addNotificationTemplate();
+
 	void updateNotificationTemplate();
 
 }
