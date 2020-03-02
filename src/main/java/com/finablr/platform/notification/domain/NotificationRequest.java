ed9fc8535ddb385df294d4ebc3251fd6fa1ec9a0
Ronak Chaudhari <ronak.chaudhari@peaas.co>
2020-03-02 12:04:46 +0530

    projectSetup completed with liquibase

--- src/main/java/com/finablr/platform/notification/domain/NotificationRequest.java
+++ src/main/java/com/finablr/platform/notification/domain/NotificationRequest.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
 @Entity
 @Table(name = "notification_request")
 public class NotificationRequest {
