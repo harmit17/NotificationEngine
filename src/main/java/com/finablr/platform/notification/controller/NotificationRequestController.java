d5b743787dcc5a74db0a54351ddfb713c7662555
Ronak Chaudhari <ronak.chaudhari@peaas.co>
2020-03-03 15:31:16 +0530

    conflict rebase completed

--- src/main/java/com/finablr/platform/notification/controller/NotificationRequestController.java
+++ src/main/java/com/finablr/platform/notification/controller/NotificationRequestController.java
@@ -1,18 +1,11 @@
 package com.finablr.platform.notification.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
-=======
-import org.springframework.web.bind.annotation.ResponseBody;
->>>>>>> Dto package created
 import org.springframework.web.bind.annotation.RestController;
 
-import com.finablr.platform.notification.domain.NotificationRequest;
 import com.finablr.platform.notification.dto.GetNotificationRequestStatusDto;
 import com.finablr.platform.notification.service.NotificationRequestService;
 
@@ -34,7 +27,7 @@ public class NotificationRequestController {
 	/*
 	 * get notification status controller method
 	 */
-	@ResponseBody
+	
 	@GetMapping("/api/v1/notification-requests/status/{id}")
 	public String getNotificationStatus(@PathVariable Long id) {
 		return service.getStatus(id);
--- src/main/java/com/finablr/platform/notification/domain/NotificationRequest.java
+++ src/main/java/com/finablr/platform/notification/domain/NotificationRequest.java
@@ -9,14 +9,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> Dto package created
 @Entity
 @Table(name = "notification_request")
 public class NotificationRequest {
