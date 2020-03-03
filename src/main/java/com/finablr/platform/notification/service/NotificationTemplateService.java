2e5999bc4bbc4549f34823198963db1108b4575b
Kartik Dambre <kartik.dambre@gmail.com>
2020-03-03 22:09:20 +0530

    Created API for getting all notification templates with services

--- src/main/java/com/finablr/platform/notification/service/NotificationTemplateService.java
+++ src/main/java/com/finablr/platform/notification/service/NotificationTemplateService.java
@@ -1,9 +1,16 @@
 package com.finablr.platform.notification.service;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.finablr.platform.notification.dto.GetAllNotificationTemplatesDto;
+
 public interface NotificationTemplateService {
 
 	void addNotificationTemplate();
 
 	void updateNotificationTemplate();
 
+	Page<GetAllNotificationTemplatesDto> getAllNotificationTemplates(Pageable pageable);
 }
--- src/main/java/com/finablr/platform/notification/service/impl/NotificationTemplateServiceImpl.java
+++ src/main/java/com/finablr/platform/notification/service/impl/NotificationTemplateServiceImpl.java
@@ -1,8 +1,22 @@
 package com.finablr.platform.notification.service.impl;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import com.finablr.platform.notification.domain.NotificationTemplate;
+import com.finablr.platform.notification.dto.GetAllNotificationTemplatesDto;
+import com.finablr.platform.notification.repository.NotificationTemplateRepository;
 import com.finablr.platform.notification.service.NotificationTemplateService;
 
 public class NotificationTemplateServiceImpl implements NotificationTemplateService{
+	
+	@Autowired
+	NotificationTemplateRepository notificationTemplateRepository;
 
 	@Override
 	public void addNotificationTemplate() {
@@ -14,6 +28,19 @@ public class NotificationTemplateServiceImpl implements NotificationTemplateServ
 	public void updateNotificationTemplate() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@SuppressWarnings("null")
+	@Override
+	public Page<GetAllNotificationTemplatesDto> getAllNotificationTemplates(Pageable pageable) {
+		Page<NotificationTemplate> notificationTemplates = notificationTemplateRepository.findAll(pageable);
+		Iterator<NotificationTemplate> notificationTemplatesIterator = notificationTemplates.iterator();
+		List<GetAllNotificationTemplatesDto> getAllNotificationTemplatesDtos = null;
+		while(notificationTemplatesIterator.hasNext()) {
+			getAllNotificationTemplatesDtos.add(new GetAllNotificationTemplatesDto(notificationTemplatesIterator.next()));	
+		}
+		Page<GetAllNotificationTemplatesDto> notificationTemplatesPage = new PageImpl<>(getAllNotificationTemplatesDtos);
+		return notificationTemplatesPage;
 	}
 	
 }
