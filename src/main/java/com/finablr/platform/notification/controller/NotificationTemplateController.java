2fcdb5e1fe5091a52c446dee3eba95b216a29bbc
Kartik Dambre <kartik.dambre@gmail.com>
2020-03-05 11:51:42 +0530

    creted branch from development

--- src/main/java/com/finablr/platform/notification/controller/NotificationTemplateController.java
+++ src/main/java/com/finablr/platform/notification/controller/NotificationTemplateController.java
@@ -1,9 +1,11 @@
 package com.finablr.platform.notification.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.finablr.platform.notification.service.NotificationTemplateService;
+import com.finablr.platform.notification.util.Response;
 
 @RestController
 public class NotificationTemplateController {
@@ -21,5 +23,6 @@ public class NotificationTemplateController {
 	{
 		notificationTemplateService.updateNotificationTemplate();
 	}
+	
 
 }
