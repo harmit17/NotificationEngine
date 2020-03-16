fd9924ae415f2d4a43933c5287b94dabfd18e252
Kartik Dambre <kartik.dambre@gmail.com>
2020-03-16 15:44:55 +0530

    resolve threads in download notification template API

--- src/main/java/com/finablr/platform/notification/util/MergePlaceHolder.java
+++ src/main/java/com/finablr/platform/notification/util/MergePlaceHolder.java
@@ -2,9 +2,14 @@ package com.finablr.platform.notification.util;
 
 import java.util.Map;
 
+import com.finablr.platform.notification.exceptionhandler.model.DataNotFoundException;
+
 public class MergePlaceHolder {
 		
 	public static String replacePlaceholders(Map<String, String> templateData, String templateBody) {
+		if(templateData==null || templateBody==null) {
+			throw new DataNotFoundException("Found null objects");		//will change to proper exception class
+		}
 		if(templateData.isEmpty()||templateBody.isEmpty()) {
 			return templateBody;
 		}
